5531f15a3bb0d69f32dcc7d783cde490bb140f4a
Anna Calinawan <anna.pamela@gmail.com>
2017-02-10 11:51:17 -0500

    Connected to the view

---
+++ browser/react/containers/LyricsContainer.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import store from '../store';
+import Lyrics from '../components/Lyrics'
+
+export default class LyricsContainer extends Component {
+	constructor() {
+		super();
+		this.state = Object.assign({
+			artistQuery: '',
+			songQuery: ''
+		}, store.getState());
+
+		// Bind functions here
+		this.handleArtistInput = this.handleArtistInput.bind(this);
+		this.handleSongInput = this.handleSongInput.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleArtistInput(artist) {
+		this.setState({
+			artistQuery: artist
+		})
+	}
+
+	handleSongInput(song) {
+		this.setState({
+			songQuery: song
+		})
+	}
+
+	handleSubmit() {
+		console.log(this.state);
+	}
+
+	componentDidMount() {
+		this.unsubscribe = store.subscribe(() => {
+			this.setState(store.getState());
+		}
+		);
+	}
+
+	componentWillUnmount() {
+		this.unsubscribe();
+	}
+
+	render() {
+		return(
+			<Lyrics 
+				text={this.state.text}
+				setArtist={this.handleArtistInput}
+				setSong = {this.handleSongInput}
+				artistQuery={this.state.artistQuery}
+				songQuery={this.state.songQuery}
+				handleSubmit={this.handleSubmit}/>
+		);
+	}
+}
